29f47c3ae3c816c141a56ddbd2902deec8652d20
Michio Nakagawa <michio.nakagawa@gmail.com>
2015-08-30 00:57:29 +0900

    add haskell-mode

---
+++ emacs.d/conf/20-haskell.el
@@ -0,0 +1,19 @@
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+(autoload 'haskell-mode "haskell-mode" nil t)
+
+(autoload 'haskell-cabal "haskell-cabal" nil t)
+ 
+(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
+(add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
+
+;; indent の有効.
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'font-lock-mode)
+(add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+
+(add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
+(add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
+
+(setq haskell-program-name "/usr/bin/ghci")
