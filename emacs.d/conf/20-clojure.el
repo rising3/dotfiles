fc5b6d6413ffb6ebe8487453d38bcc69815835ad
Michio Nakagawa <michio.nakagawa@gmail.com>
2015-11-25 02:19:44 +0900

    add cider

--- emacs.d/conf/20-clojure.el
+++ emacs.d/conf/20-clojure.el
@@ -1,3 +1,15 @@
-(require 'clojure-mode)
-(defun turn-on-paredit () (paredit-mode 1))
-(add-hook  'clojure-mode-hook 'turn-on-paedit)
+;(require 'clojure-mode)
+;(defun turn-on-paredit () (paredit-mode 1))
+;(add-hook  'clojure-mode-hook 'turn-on-paedit)
+
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+
+(add-hook 'cider-mode-hook #'eldoc-mode)
+(setq cider-auto-mode nil)
+(setq nrepl-log-messages nil)
+(setq nrepl-hide-special-buffers t)
+
+
