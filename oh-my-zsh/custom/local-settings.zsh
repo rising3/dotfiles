b86e81c57ed1c339754052b9667b7ca228571d97
Michio Nakagawa <michio.nakagawa@gmail.com>
2016-01-02 15:49:56 +0900

    fixed

--- oh-my-zsh/custom/local-settings.zsh
+++ oh-my-zsh/custom/local-settings.zsh
@@ -6,5 +6,7 @@ plugins=(gitfast ruby gem bundler rails emoji-clock brew osx)
 
 # aliases
 alias emacs='emacsclient -nw'
+alias kill-emacs='emacsclient -e "(kill-emacs)"'
+
 # startup tmux
 [[ -z "$TMUX" && ! -z "$PS1" ]] && tmux new-session \; source-file ~/.tmux.d/new-session
