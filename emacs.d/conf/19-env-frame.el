6c620bc972895440258b884e3078ebcf6a93a7c0
michio nakagawa <michio.nakagawa@gmail.com>
2016-06-08 00:03:40 +0900

    change color theme

--- emacs.d/conf/19-env-frame.el
+++ emacs.d/conf/19-env-frame.el
@@ -3,8 +3,9 @@
 ;;(enable-theme 'subdued)
 (when (require 'color-theme nil t)
   (color-theme-initialize)
-  (color-theme-dark-laptop)
+  (color-theme-monokai)
   )
+  ;;(color-theme-dark-laptop)
   ;;(color-theme-charcoal-black)
   ;;(color-theme-billw)
   ;;(color-theme-oswald)
@@ -27,11 +28,11 @@
 (if (boundp 'window-system)
     (setq default-frame-alist
           (append (list
-;;                   '(foreground-color . "black")  ; 文字色
-;;                   '(background-color . "white")  ; 背景色
+                   '(foreground-color . "white")  ; 文字色
+                   '(background-color . "black")  ; 背景色
 ;;                   '(border-color     . "white")  ; ボーダー色
 ;;                   '(mouse-color      . "black")  ; マウスカーソルの色
-;;                   '(cursor-color     . "black")  ; カーソルの色
+                   '(cursor-color     . "white")  ; カーソルの色
                    '(cursor-type      . box)      ; カーソルの形状
                    ;;'(top . 60) ; ウィンドウの表示位置（Y座標）
                    ;;'(left . 100) ; ウィンドウの表示位置（X座標）
