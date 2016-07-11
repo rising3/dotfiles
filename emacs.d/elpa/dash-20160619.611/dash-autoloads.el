0e162e49903ab9a840ed7f1e5e0e8324e116c26a
michio nakagawa <michio.nakagawa@gmail.com>
2016-07-12 01:18:51 +0900

    upgrade packages

---
+++ emacs.d/elpa/dash-20160619.611/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (22403 50652 157032 101000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
