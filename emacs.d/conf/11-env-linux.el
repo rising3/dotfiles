82b024bcfe09e190673aca687db5d5c3e5298675
Michio Nakagawa <michio.nakagawa@gmail.com>
2018-10-18 23:48:10 +0900

    polish.

--- emacs.d/conf/11-env-linux.el
+++ emacs.d/conf/11-env-linux.el
@@ -21,7 +21,7 @@
   ;; フォント設定
   (set-face-attribute 'default nil
 		      :family "Ricty Discord"
-		      :height 120)
+		      :height 180)
 
   (set-fontset-font (frame-parameter nil 'font)
 		    'japanese-jisx0208
